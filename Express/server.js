bbb7e187571cf76431376006ca5fafa641c34a02
Çağatay Yıldız <yildiz.cagatay@hotmail.com>
2020-05-17 18:13:18 +0300

    express route

--- Express/server.js
+++ Express/server.js
@@ -1,22 +1,59 @@
 const express = require('express');
 const app = express();
 
-app.get("/",function(req,res){
+app.get("/", function (req, res) {
     res.send("Hello Express!!");
 });
 
-app.get("/contact",function(req,res){
+app.get("/contact", function (req, res) {
     res.send("Contact Page");
+});
+
+app.get("/notfound", function (req, res) {
+    res.status(404).send("Page not found!!");
 })
 
 var kisi = {
-    "ad":"Çağatay",
-    "soyad":"Yıldız",
-    "sehir":"İzmir"
+    "ad": "Çağatay",
+    "soyad": "Yıldız",
+    "sehir": "İzmir"
 };
 
-app.get("/user",function(req,res){
+app.get("/user", function (req, res) {
     res.json(kisi);
+});
+
+
+app.get("/products/:id", function (req, res) {
+    var id = req.params.id;
+    res.send("Product detail page. ID " + id);
+});
+
+var metalgroups = [
+    { "id": 1, "name": "Slipnot" },
+    { "id": 2, "name": "Pentagram" },
+    { "id": 3, "name": "Iron Maiden" },
+    { "id": 4, "name": "In Flames" },
+];
+
+app.get("/groups", function (req, res) {
+    res.json(metalgroups);
+});
+
+app.get("/groups/:id", function (req, res) {
+
+    var groupid = req.params.id;
+    var data = metalgroups.find(q => q.id == groupid);
+
+    if (data == undefined) {
+        res.status(404).send("Sayfa bulunamadı!");
+    }
+    else {
+        res.json(data);
+    }
+
 })
+
+
 
 app.listen(3000);
