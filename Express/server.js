f4a9fa1aa80e71a27713dc4056a306348a8557f0
Çağatay Yıldız <yildiz.cagatay@hotmail.com>
2020-05-17 18:38:51 +0300

    express örnek

--- Express/server.js
+++ Express/server.js
@@ -35,24 +35,43 @@ var metalgroups = [
     { "id": 3, "name": "Iron Maiden" },
     { "id": 4, "name": "In Flames" },
 ];
-
+//yukarıdaki gibi bir json obje tanımla. sonra bu objeyi paslayacak bir api oluştur. (aşağıdaki gibi. daha sonra id ye göre elemanı döndürecek apiyi yaz
 app.get("/groups", function (req, res) {
     res.json(metalgroups);
 });
-
 app.get("/groups/:id", function (req, res) {
-
     var groupid = req.params.id;
     var data = metalgroups.find(q => q.id == groupid);
-
     if (data == undefined) {
         res.status(404).send("Sayfa bulunamadı!");
     }
     else {
         res.json(data);
     }
+});
 
+
+var ressamlar = [
+        {"id":1,"name":"Picasso"},
+        {"id":2,"name":"Van Gogh"},
+        {"id":3,"name":"Gaugin"},
+        {"id":4,"name":"Osman Hamdi Bey"},
+];
+
+app.get("/painters",function(req,res){
+    res.json(ressamlar);
 })
+
+app.get("/painters/:id",function(req,res){
+    var id = req.params.id;
+
+    var ressam = ressamlar.find(q => q.id == id);
+    res.json(ressam);
+})
+
+
+
+
 
 
 
