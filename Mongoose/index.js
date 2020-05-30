f038fa5ca7306b78897ab3017b0a99714878d7a4
Çağatay Yıldız <yildiz.cagatay@hotmail.com>
2020-05-30 18:19:46 +0300

    mongoose

--- Mongoose/index.js
+++ Mongoose/index.js
@@ -2,6 +2,12 @@ const mongoose = require("mongoose");
 const express = require('express');
 const app = express();
 
+var bodyParser = require("body-parser");
+
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
+
 mongoose.connect(`mongodb+srv://azra_elturco:1996417B@cluster417-vl3kd.mongodb.net/test?authSource=admin&replicaSet=Cluster417-shard-0&w=majority&readPreference=primary&appname=MongoDB%20Compass&retryWrites=true&ssl=true
 `, { useNewUrlParser: true });
 
@@ -22,8 +28,14 @@ const writerSchema = new Schema({
     favoritebook:String
 });
 
+const comicbookSchema = new Schema({
+    name:String,
+    country:String
+})
+
 const writer = mongoose.model("Writer",writerSchema);
 const metal = mongoose.model("Metal",metalSchema);
+const comicbook = mongoose.model("Comicbook",comicbookSchema);
 
 
 app.get("/api/writer",(req,res)=>{
@@ -39,6 +51,37 @@ app.get("/api/writer/:id",(req,res)=>{
     })
 });
 
+app.post("/api/writer",(req,res)=>{
+
+    var wr = new writer({
+        name: req.body.name,
+        surname:req.body.surname,
+        city:req.body.city,
+        favoritebook:req.body.fbook
+    });
+    wr.save();
+
+    res.send("Success!!");
+})
+
+
+
+app.get("/api/comicbook",(req,res)=>{
+    comicbook.find((err,doc)=>{
+        res.json(doc);
+    })
+})
+
+app.post("/api/comicbook",(req,res)=>{
+    var cb = new comicbook({
+        name:req.body.name,
+        country:req.body.country
+    });
+
+    cb.save();
+
+    res.send("Success!!")
+})
 
 app.listen(3001)
 
@@ -53,6 +96,8 @@ writer.find({"city":"Adana"},(err,doc)=>{
 
 
 
+
+
 // 2 adet yeni schema olustur.
 //1 - product (name, unitprice(number olacak), description)
 //2 - employee (name, surname, birthdate)
@@ -60,11 +105,6 @@ writer.find({"city":"Adana"},(err,doc)=>{
 //bu şemalara test verileri gir
 
 //2 adet express uç oluştur. "/api/product" ve "/api/employee" şeklinde. Bu url ler tetiklendiğinde datalar gelsin
-
-
-
-
-
 
 // var wr = new writer({
 //     name:"H",
@@ -79,6 +119,13 @@ writer.find({"city":"Adana"},(err,doc)=>{
 //     country:"Deutchland",
 //     songs:['Deutchland','Radio','America']
 // });
+
+var cb = new comicbook({
+    name:"Watchmen",
+    country:"USA"
+});
+
+//cb.save();
 
 
 //mt.save();
